21e05ff700d6161ce03fb066d471dc7af34f7d46
Preston Smith <preston.s1993@gmail.com>
2018-05-25 12:14:04 -0500

    Replay: Initial commit
    This is not ready for public use. Do not use it.

--- src/Client/Settings.java
+++ src/Client/Settings.java
@@ -189,6 +189,8 @@ public class Settings {
 		Util.makeDirectory(Dir.CACHE);
 		Dir.SCREENSHOT = Dir.JAR + "/screenshots";
 		Util.makeDirectory(Dir.SCREENSHOT);
+		Dir.REPLAY = Dir.JAR + "/replay";
+		Util.makeDirectory(Dir.REPLAY);
 	}
 	
 	/**
@@ -833,6 +835,7 @@ public class Settings {
 		public static String CACHE;
 		public static String DUMP;
 		public static String SCREENSHOT;
+		public static String REPLAY;
 	}
 	
 	public static String[] WORLD_LIST = { "1", "2", "3", "4", "5" };
